7d1cf5edda08639a6b7adda5f43fa96d168921e9
Dougal Graham <dougalg@gmail.com>
2016-10-11 23:00:18 -0230

    More thorough tests
    Test multi-blok bloks
    Add missing test case for ensuring blocks always touch

--- test/board_test.js
+++ test/board_test.js
@@ -20,7 +20,7 @@ describe('game board', function() {
             throws(() => new Board(undefined));
         });
     });
-    describe('placing a block', function() {
+    describe('validates placing a block such that it', function() {
         let board;
         let block;
         beforeEach(function() {
@@ -42,16 +42,33 @@ describe('game board', function() {
             block = new Block(0, [ [ 0, 0 ], [ 1, 0 ] ]);
             board.place(block, [ 0, 0 ]);
         });
-        it('ensures a non-first block is played such that it doesn\'t overlap other blocks', function() {
+        it('ensures a block is played such that it doesn\'t overlap other blocks', function() {
             board.place(block, [ 0, 0 ]);
             throws(() => board.place(block, [ 0, 0 ]));
             board.place(block, [ 1, 1 ]);
             throws(() => board.place(block, [ 1, 1 ]));
+            block = new Block(0, [ [ 0, 0 ], [ 1, 0 ] ]);
+            throws(() => board.place(block, [ 0, 0 ]));
         });
-        it('ensures a non-first block is played such that it does not have touching edges with this user\'s other blocks', function() {
+        it('ensures a block is played such that it does not have touching edges with this user\'s other blocks', function() {
             board.place(block, [ 0, 0 ]);
             throws(() => board.place(block, [ 0, 1 ]));
             throws(() => board.place(block, [ 1, 0 ]));
+            block = new Block(0, [ [ 0, 0 ], [ 1, 0 ] ]);
+            throws(() => board.place(block, [ 0, 1 ]));
+        });
+        it('ensures that blocks always touch a related block in the corner', function() {
+            board.place(block, [ 0, 0 ]);
+            board.place(block, [ 1, 1 ]);
+            throws(() => board.place(block, [ 3, 3 ]));
+        });
+        it('does not allow blocks placed outside the board', function() {
+            throws(() => board.place(block, [ -1, 0 ]));
+            throws(() => board.place(block, [ 0, -1 ]));
+            throws(() => board.place(block, [ 20, 0 ]));
+            throws(() => board.place(block, [ 0, 20 ]));
+            block = new Block(0, [ [ 0, 0 ], [ 1, 0 ] ]);
+            throws(() => board.place(block, [ 19, 0 ]));
         });
         it('allows blocks to be placed that do not violate above rules', function() {
             board.place(block, [ 0, 0 ]);
@@ -59,12 +76,9 @@ describe('game board', function() {
             board.place(block, [ 2, 2 ]);
             board.place(block, [ 0, 2 ]);
             board.place(block, [ 2, 0 ]);
-        });
-        it('does not allow blocks placed outside the board', function() {
-            throws(() => board.place(block, [ -1, 0 ]));
-            throws(() => board.place(block, [ 0, -1 ]));
-            throws(() => board.place(block, [ 20, 0 ]));
-            throws(() => board.place(block, [ 0, 20 ]));
+            block = new Block(0, [ [ 0, 0 ], [ 1, 0 ] ]);
+            board.place(block, [ 3, 3 ]);
+            board.place(block, [ 5, 4 ]);
         });
     });
 });
